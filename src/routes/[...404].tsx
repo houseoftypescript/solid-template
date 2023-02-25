09e190840c02c1c85ecc71c8f4bcbe9df9adf58c
hieudoanm <hieumdoan@gmail.com>
2023-02-25 22:34:47 +0700

    initial

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <main class="w-full h-screen flex flex-col justify-center items-center gap-4">
+      <h1 class="uppercase text-9xl font-bold">404</h1>
+      <p class="uppercase text-3xl">Page Not Found</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
