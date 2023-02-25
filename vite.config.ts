f03ec866a25dd1bf31f92f5bfedf4fe678a1c888
hieudoanm <hieumdoan@gmail.com>
2023-02-25 23:42:22 +0700

    Fix base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: isProd ? '/solid-template' : '',
   server: {
     port: 3000,
   },
