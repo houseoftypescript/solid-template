d8b30e89572d16100679d423170d8e06da74ead3
hieudoanm <hieumdoan@gmail.com>
2023-02-26 11:07:33 +0700

    Fix base for Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import type { Component } from 'solid-js';
 import IndexPage from './pages';
 import NotFoundPage from './pages/404';
 
+const isProd: boolean = process.env.NODE_ENV === 'production';
+const base: string = isProd ? '/solid-template' : '';
+
 const App: Component = () => {
   return (
-    <Routes>
+    <Routes base={base}>
       <Route path="/" component={IndexPage} />
       <Route path="*" component={NotFoundPage} />
     </Routes>
